ac3a199b2a76cd8ed32f02f412cf65bff10caa77
atchopba <atchopba@gmail.com>
2020-04-24 00:55:31 +0200

    Refactoring code

--- static/js/site.js
+++ static/js/site.js
@@ -10,7 +10,8 @@ $(document).ready(function(){
             return;
         }
 
-        $("#boxresult").html("recherche en cours ...");
+        $("#boxresult").css("display", "none");
+        $("#boxloading").css("display", "block");
 
     	$.ajax({
     		url: "/search",
@@ -19,9 +20,25 @@ $(document).ready(function(){
     			q: search_text
     		},
     		success : function(data) {
-    			$("#boxresult").html(data);
+    			tweets = JSON.parse(data);
+    			console.log(tweets);
+    			_html = "";
+    			if (tweets && tweets.length > 0) {
+        			tweets.forEach(function(t) {
+            			_html += "<tr><td>"+ t[0] +"</td><td>"+ t[1] +"</td><td>"+ t[2] +"</td><td>"+ t[3] +"</td><td>"+ t[4] +"</td><td>"+ t[5] +"</td><td>"+ t[6] +"</td></tr>";
+        			});
+    			} else {
+        			_html = "<tr><td colspan='7'>No results!</td></tr>";
+    			}
+    			//
+        		$("#boxloading").css("display", "none");
+    			$("#boxresult").css("display", "block");
+    			$(".tbody_result").html(_html);
+    			//console.log(_html);
+    			//$("#boxresult").html(data);
     		},
     		error: function(result, status, error) {
+        		$("#boxloading").css("display", "none");
 				alert("Une erreur est survenue. Veuillez ressayer!");
 			}
     	});
--- treatment/usertweets.py
+++ treatment/usertweets.py
@@ -12,7 +12,10 @@
 
 import tweepy
 import config
+from collections import namedtuple
+import json
 
+Tweet = namedtuple("Tweet", "i id_str name text location created_at retweet_count")
 
 def search_tweets(q):
     auth = tweepy.OAuthHandler(config.CONSUMER_KEY, config.CONSUMER_SECRET)
@@ -20,33 +23,19 @@ def search_tweets(q):
     auth.set_access_token(config.ACCESS_TOKEN, config.ACCESS_SECRET)
     api = tweepy.API(auth)
     #
-    return_html = ("<br/><h3>Résultats</h3>"
-       "<table border='1'>"
-            "<tr>"
-                "<td></td>"
-                "<td>id str</td>"
-                "<td>username</td>"
-                "<td>text</td>"
-                "<td>location</td>"
-                "<td>created at</td>"
-                "<td>nb retweet</td>"
-            "</tr>"    
-    "")
-    #
     i = 1
+    return_dict = []
     for t in api.search(q, count=100):
-        return_html += (""
-            "<tr>"
-                "<td>"+ str(i) +"</td>"
-                "<td>"+ t._json["id_str"] +"</td>"
-                "<td><a href='http://www.twitter.com/"+ t._json["user"]["screen_name"] +"' target='_blank'>"+ t._json["user"]["name"] +"</a> </td>"
-                "<td>"+ t._json["text"] +"</td>"
-                "<td>"+ t._json["user"]["location"] +"</td>"
-                "<td>"+ t._json["created_at"] +"</td>"
-                "<td>"+ str(t._json["retweet_count"]) +"</td>"
-        "</tr>")
-        i +=1
-    if (i == 1):
-        return_html += "<td colspan='7'>No result !</td>"
-    return_html += "</tboby></table>"
-    return return_html
+        return_dict.append(
+            Tweet(str(i), 
+                  t._json["id_str"],
+                  "<a href='http://www.twitter.com/"+ t._json["user"]["screen_name"] +"' target='_blank'>"+ t._json["user"]["name"] +"</a> ",
+                  t._json["text"],
+                  t._json["user"]["location"],
+                  t._json["created_at"],
+                  str(t._json["retweet_count"])
+              ))
+        i += 1
+    #if (i == 1):
+    #    return_dict.append("No result !")
+    return json.dumps(return_dict)
