ecfb410dfb4b958b9f00f9eccd5f3fa0b40b166e
albinTch <atchopba@gmail.com>
2020-06-22 00:49:26 +0200

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# __author__ = "Albin TCHOPBA"
+# __copyright__ = "Copyright 2020 Albin TCHOPBA and contributors"
+# __credits__ = ["Albin TCHOPBA and contributors"]
+# __license__ = "GPL"
+# __version__ = "3"
+# __maintainer__ = "Albin TCHOPBA"
+# __email__ = "Albin TCHOPBA <atchopba @ gmail dot com"
+# __status__ = "Production"
+
+export FLASK_APP=wsgi.py
+export FLASK_DEBUG=1
+export APP_CONFIG_FILE=config.py
+
+flask run
